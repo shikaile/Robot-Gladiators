1dbfdb61bd1a9c3edd2de91255379105a872b3c2
史开乐 <csurfssecure@gmail.com>
2022-04-18 12:53:11 -0400

    begining js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("this is a message?");
+var playerName = window.prompt("What is your robot's name?");
+
+var playerHealth = 100;
+
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = Roborto;
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function(){
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
+
