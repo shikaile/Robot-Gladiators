af36c99e28ce9a36d8de7ef33fd161c7ab1fd98f
Stokes <calebstokes@rocketmortgage.com>
2022-04-21 16:36:32 -0400

    name bug resolved

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,7 +79,7 @@ for(var i =0; i < enemyInfo.length; i++){
   if (playerInfo.health > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
+
     // pick new enemy to fight based on the index of the enemy.names array
       var pickedEnemyObj = enemyInfo[i];
 
@@ -158,8 +158,17 @@ switch (shopOptionPrompt) {
 };
 // function defenition to generate random numeric value
 
+var getPlayerName = function(){
+  var name = "";
+  
+  while (name === "" || name === null){
+    name = prompt("What is your robot name?");
+  }
+  console.log("your robot's name is " + name);
+  return name;
+};
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 30,
   money: 10,
